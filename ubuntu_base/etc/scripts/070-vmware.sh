5a95e10555a2e1d4fb428ddb780cd08b9771218d
David Bond <davidebond@gmail.com>
2021-04-12 20:17:24 +0200

    Change vmware service start order

--- ubuntu_base/etc/scripts/070-vmware.sh
+++ ubuntu_base/etc/scripts/070-vmware.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -eux
-
+#https://kb.vmware.com/s/article/59687 - SED section, to ensure dbus starts before vmware tools
 case "$PACKER_BUILDER_TYPE" in
-vmware-iso|vmware-vmx)
+vmware-iso|vmware-vmx|vsphere-iso|vsphere-clone)
     apt-get install -y open-vm-tools;
     mkdir /mnt/hgfs;
+    sed '/\[Unit\]/a After=dbus.service' /lib/systemd/system/open-vm-tools.service > /tmp/open-vm-tools.service
+    sudo cp /tmp/open-vm-tools.service /lib/systemd/system/open-vm-tools.service
+    rm /tmp/open-vm-tools.service
     systemctl enable open-vm-tools
     systemctl start open-vm-tools
     echo "platform specific vmware.sh executed";
